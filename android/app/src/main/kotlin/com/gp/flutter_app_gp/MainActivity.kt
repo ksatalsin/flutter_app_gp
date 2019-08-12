4fc952f84bb46921d6e58c0b4393d73fe32f506b
ksatalsin <ksatalsin@gmail.com>
2019-08-12 12:53:47 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/gp/flutter_app_gp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gp.flutter_app_gp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
